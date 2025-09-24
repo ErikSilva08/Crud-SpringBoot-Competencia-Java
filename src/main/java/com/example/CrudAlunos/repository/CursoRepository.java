d89b180e59b169f1086b4afbd3e7bde56b100b0b
Edu88888 <sousapereiraluizeduardo@gmail.com>
2025-09-23 23:52:57 -0300

    Create CursoRepository.java

---
+++ src/main/java/com/example/CrudAlunos/repository/CursoRepository.java
@@ -0,0 +1,6 @@
+package com.example.CrudAlunos.repository;
+
+import com.example.CrudAlunos.entity.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {}
