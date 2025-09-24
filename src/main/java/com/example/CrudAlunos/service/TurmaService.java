b61c00fde657d3f0728aa7633e63ed7bc6eb421d
CaioBrunor <caiobrunorodrigues2006@gmail.com>
2025-09-24 02:17:01 -0300

    Commit de Caio

--- src/main/java/com/example/CrudAlunos/service/TurmaService.java
+++ src/main/java/com/example/CrudAlunos/service/TurmaService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor // elimina o @Autowired
+@RequiredArgsConstructor
 public class TurmaService {
 
     private final TurmaRepository turmaRepository;
